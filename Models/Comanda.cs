0dc4d972b3e13943bd0deccdd8046feec7e16b5f
Emílo Holgado da Costa <emiliocosta@portalsesisp.org.br>
2023-10-09 16:11:50 -0300

    layout

--- Models/Comanda.cs
+++ Models/Comanda.cs
@@ -7,7 +7,7 @@ namespace AlMocar.Models
          [Display(Name ="Informe o nome do lanche")]
         [Required(ErrorMessage ="Informe o nome do lanche")]
         [MinLength(3, ErrorMessage ="Nome do lanche deve ter no minimo {1} caracteries")]
-        [MaxLength(25, ErrorMessage ="Nome do lanche deve ter no minimo {1} caracteries")]
+        [MaxLength(125, ErrorMessage ="Nome do lanche deve ter no minimo {1} caracteries")]
         public int Lanche{get; set;}
     }
 }
